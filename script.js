3e46937bf048cee347dd1511eb4897659ef1f56e
Jojordanme <135418765+Jojordanme@users.noreply.github.com>
2026-04-16 03:31:52 +0000

    my name is anderdingus

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ var modal = document.getElementById('myModal');
 var span = document.getElementsByClassName("close")[0];
 
 modelfungo = function() {
-    console.log("func called")
     modal.style.display = "block";
     x = document.querySelector(".gamehead");
     x.textContent = "Game Over"
-
 }
+
 modelfunwin = function() {
-    console.log("func called")
     modal.style.display = "block";
     x = document.querySelector(".gamehead");
     x.textContent = "Congratulations! You Win"
-
 }
 
 document.getElementById("demo").addEventListener("click", myFunction);
@@ -21,23 +18,47 @@ document.getElementById("demo").addEventListener("click", myFunction);
 function myFunction() {
     document.location.reload();
 }
-span.onclick = function() {
-    modal.style.display = "none";
+
+var playing = false; // Start as false
+
+// Elements
+const mainMenu = document.getElementById('main-menu');
+const startBtn = document.getElementById('start-btn');
+const gameContainer = document.getElementById('game-container');
+
+// Start Button Event
+startBtn.addEventListener('click', function() {
+    mainMenu.style.display = 'none';
+    gameContainer.style.display = 'block';
+    initGame();
+});
+
+function initGame() {
+    playing = true;
+    m = new maze(10, 10);
+    m.init();
+    m.add_edges();
+    m.gen_maze();
+    m.draw_canvas("canvas");
+
+    // Start the timer
+    var countdownTime = 30; // 30 seconds
+    var timerDisplay = document.querySelector("#timerel");
+    startTimer(countdownTime, timerDisplay);
+
+    // Start drawing moves
+    setInterval(drawMoves, 100);
 }
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+
+// Update your startTimer function to ensure it stops correctly
 function startTimer(duration, display) {
-    var start = Date.now(),
-        diff,
-        minutes,
-        seconds;
+    var start = Date.now(), diff, minutes, seconds;
 
     function timer() {
         if (playing) {
             diff = duration - (((Date.now() - start) / 1000) | 0);
+            if (diff < 0) diff = 0; // Prevent negative time
+
             minutes = (diff / 60) | 0;
             seconds = (diff % 60) | 0;
             minutes = minutes < 10 ? "0" + minutes : minutes;
@@ -45,69 +66,120 @@ function startTimer(duration, display) {
             display.textContent = "Time Left " + minutes + ":" + seconds;
 
             if (diff <= 0) {
-                display.textContent = "Game Over";
-                start = Date.now() + 1000;
-                playing = false
+                playing = false;
                 modelfungo();
+                clearInterval(timerInterval);
             }
         }
     };
     timer();
-    setInterval(timer, 1000)
+    var timerInterval = setInterval(timer, 1000);
 }
 
-window.onload = function() {
-    twominutes = 15;
-    x = document.querySelector("#timerel");
-    startTimer(twominutes, x)
-}
-playing = true
+// Remove the old window.onload and m = new maze() from the bottom of your script 
+// so they don't run automatically.
+
+// --- KEYBOARD CONTROLS (Desktop) ---
 window.addEventListener('keydown', doKeyDown, true);
 
 function doKeyDown(evt) {
     var handled = false;
     if (playing) {
         switch (evt.keyCode) {
-            case 38:
-                m.moveup("canvas");
-                handled = true
-                break;
-            case 87:
-                m.moveup("canvas");
-                handled = true
-                break;
-            case 40:
-                m.movedown("canvas");
-                handled = true
-                break;
-            case 83:
-                m.movedown("canvas");
-                handled = true
-                break;
-            case 37:
-                m.moveleft("canvas");
-                handled = true
-                break;
-            case 65:
-                m.moveleft("canvas");
-                handled = true
-                break;
-            case 39:
-                m.moveright("canvas");
-                handled = true
-                break;
-            case 68:
-                m.moveright("canvas");
-                handled = true
-                break;
+            case 38: case 87: m.moveup("canvas"); handled = true; break;
+            case 40: case 83: m.movedown("canvas"); handled = true; break;
+            case 37: case 65: m.moveleft("canvas"); handled = true; break;
+            case 39: case 68: m.moveright("canvas"); handled = true; break;
         }
-        if (m.checker("canvas"))
-            playing = false
-        console.log(m.getMoves())
+        if (m.checker("canvas")) playing = false;
     }
-    if (handled)
-        evt.preventDefault();
+    if (handled) evt.preventDefault();
 }
+
+// --- VIRTUAL JOYSTICK LOGIC (Mobile) ---
+const joystickContainer = document.getElementById('joystick-container');
+const joystickKnob = document.getElementById('joystick-knob');
+
+let joystickInterval = null;
+let currentDirection = null;
+const maxRadius = 35; // Maximum pixel distance the knob can move from center
+
+function handleJoystick(e) {
+    e.preventDefault();
+    if (!playing) return;
+
+    let touch = e.targetTouches[0];
+    let rect = joystickContainer.getBoundingClientRect();
+
+    // Calculate distance from center of the joystick container
+    let centerX = rect.width / 2;
+    let centerY = rect.height / 2;
+    let dx = touch.clientX - rect.left - centerX;
+    let dy = touch.clientY - rect.top - centerY;
+
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    // Constrain knob inside the circle
+    if (distance > maxRadius) {
+        dx = (dx / distance) * maxRadius;
+        dy = (dy / distance) * maxRadius;
+    }
+
+    // Move the knob visually
+    joystickKnob.style.transform = `translate(${dx}px, ${dy}px)`;
+
+    // Calculate angle to determine direction (deadzone of 10px)
+    if (distance > 10) {
+        let angle = Math.atan2(dy, dx) * (180 / Math.PI);
+
+        if (angle > -45 && angle <= 45) currentDirection = 'RIGHT';
+        else if (angle > 45 && angle <= 135) currentDirection = 'DOWN';
+        else if (angle > 135 || angle <= -135) currentDirection = 'LEFT';
+        else if (angle > -135 && angle <= -45) currentDirection = 'UP';
+
+        // Start moving if not already looping
+        if (!joystickInterval) {
+            executeJoystickMove(); // Move immediately
+            joystickInterval = setInterval(executeJoystickMove, 150); // Keep moving every 150ms
+        }
+    } else {
+        resetJoystick();
+    }
+}
+
+function executeJoystickMove() {
+    if (!playing) return resetJoystick();
+
+    if (currentDirection === 'UP') m.moveup("canvas");
+    else if (currentDirection === 'DOWN') m.movedown("canvas");
+    else if (currentDirection === 'LEFT') m.moveleft("canvas");
+    else if (currentDirection === 'RIGHT') m.moveright("canvas");
+
+    if (m.checker("canvas")) {
+        playing = false;
+        resetJoystick();
+    }
+}
+
+function resetJoystick() {
+    joystickKnob.style.transform = `translate(0px, 0px)`;
+    currentDirection = null;
+    if (joystickInterval) {
+        clearInterval(joystickInterval);
+        joystickInterval = null;
+    }
+}
+
+// Bind touch events to the joystick container
+joystickContainer.addEventListener('touchstart', handleJoystick, { passive: false });
+joystickContainer.addEventListener('touchmove', handleJoystick, { passive: false });
+joystickContainer.addEventListener('touchend', (e) => {
+    e.preventDefault();
+    resetJoystick();
+}, { passive: false });
+
+
+// --- MAZE GENERATOR LOGIC ---
 var dsd = function(size) {
     this.N = size;
     this.P = new Array(this.N);
@@ -118,35 +190,26 @@ var dsd = function(size) {
             this.R[i] = 0;
         }
     }
-
     this.union = function(x, y) {
         var u = this.find(x);
         var v = this.find(y);
         if (this.R[u] > this.R[v]) {
             this.R[u] = this.R[v] + 1;
             this.P[u] = v;
-        }
-        else {
+        } else {
             this.R[v] = this.R[u] + 1;
             this.P[v] = u;
         }
     }
-
     this.find = function(x) {
-        if (x == this.P[x])
-            return x;
+        if (x == this.P[x]) return x;
         this.P[x] = this.find(this.P[x]);
         return this.P[x];
     }
 };
 
-function random(min, max) {
-    return (min + (Math.random() * (max - min)));
-};
-
-function randomChoice(choices) {
-    return choices[Math.round(random(0, choices.length - 1))];
-};
+function random(min, max) { return (min + (Math.random() * (max - min))); };
+function randomChoice(choices) { return choices[Math.round(random(0, choices.length - 1))]; };
 
 var maze = function(X, Y) {
     this.N = X;
@@ -163,21 +226,12 @@ var maze = function(X, Y) {
             this.Board[i] = new Array(2 * this.M + 1);
             this.vis[i] = new Array(2 * this.M + 1);
         }
-
         for (var i = 0; i < 2 * this.N + 1; i++) {
             for (var j = 0; j < 2 * this.M + 1; j++) {
-                if (!(i % 2) && !(j % 2)) {
-                    this.Board[i][j] = '+';
-                }
-                else if (!(i % 2)) {
-                    this.Board[i][j] = '-';
-                }
-                else if (!(j % 2)) {
-                    this.Board[i][j] = '|';
-                }
-                else {
-                    this.Board[i][j] = ' ';
-                }
+                if (!(i % 2) && !(j % 2)) this.Board[i][j] = '+';
+                else if (!(i % 2)) this.Board[i][j] = '-';
+                else if (!(j % 2)) this.Board[i][j] = '|';
+                else this.Board[i][j] = ' ';
                 this.vis[i][j] = 0;
             }
         }
@@ -185,19 +239,12 @@ var maze = function(X, Y) {
     this.add_edges = function() {
         for (var i = 0; i < this.N; i++) {
             for (var j = 0; j < this.M; j++) {
-                if (i != this.N - 1) {
-                    this.EL.push([[i, j], [i + 1, j], 1]);
-                }
-                if (j != this.M - 1) {
-                    this.EL.push([[i, j], [i, j + 1], 1]);
-                }
+                if (i != this.N - 1) this.EL.push([[i, j], [i + 1, j], 1]);
+                if (j != this.M - 1) this.EL.push([[i, j], [i, j + 1], 1]);
             }
         }
     }
-
-    this.h = function(e) {
-        return e[1] * this.M + e[0];
-    }
+    this.h = function(e) { return e[1] * this.M + e[0]; }
     this.randomize = function(EL) {
         for (var i = 0; i < EL.length; i++) {
             var si = Math.floor(Math.random() * 387) % EL.length;
@@ -212,7 +259,6 @@ var maze = function(X, Y) {
         var y = e[0][1] + e[1][1] + 1;
         this.Board[x][y] = ' ';
     }
-
     this.gen_maze = function() {
         this.EL = this.randomize(this.EL);
         var D = new dsd(this.M * this.M);
@@ -233,19 +279,13 @@ var maze = function(X, Y) {
                         this.EL[i][2] = 0;
                     }
                 }
-            }
-
-            else if (D.find(x) != D.find(y)) {
+            } else if (D.find(x) != D.find(y)) {
                 D.union(x, y);
                 this.breakwall(this.EL[i]);
                 this.EL[i][2] = 0;
             }
-            else {
-                continue;
-            }
         }
     };
-
     this.draw_canvas = function(id) {
         this.canvas = document.getElementById(id);
         var scale = this.S;
@@ -258,9 +298,7 @@ var maze = function(X, Y) {
                     if (this.Board[i][j] != ' ') {
                         this.ctx.fillStyle = "black";
                         this.ctx.fillRect(scale * i, scale * j, scale, scale);
-                    }
-                    else if (i < 5 && j < 5)
-                        temp.push([i, j])
+                    } else if (i < 5 && j < 5) temp.push([i, j])
                 }
             }
             x = randomChoice(temp)
@@ -269,18 +307,13 @@ var maze = function(X, Y) {
             this.ctx.fillRect(scale * x[0], scale * x[1], scale, scale);
         }
     };
-
     this.checkPos = function(id) {
         for (var i = 0; i < 2 * this.N + 1; i++) {
             for (var j = 0; j < 2 * this.M + 1; j++) {
-                if (this.Board[i][j] == '&') {
-
-                    return [i, j]
-                }
+                if (this.Board[i][j] == '&') return [i, j];
             }
         }
     }
-
     this.moveclear = function(a, b) {
         var scale = this.S;
         this.ctx = this.canvas.getContext('2d');
@@ -288,7 +321,6 @@ var maze = function(X, Y) {
         this.ctx.fillRect(scale * a, scale * b, scale, scale);
         this.Board[a][b] = ' '
     }
-
     this.move = function(a, b) {
         var scale = this.S;
         this.ctx = this.canvas.getContext('2d');
@@ -296,102 +328,48 @@ var maze = function(X, Y) {
         this.ctx.fillRect(scale * a, scale * b, scale, scale);
         this.Board[a][b] = '&'
     }
-
     this.moveup = function(id) {
         cord = this.checkPos(id);
-        var scale = this.S;
-        i = cord[0]
-        j = cord[1]
-        j -= 1
-        if (j < 0)
-            return
-        else if (j > 2 * this.M)
-            return
-        else if (this.Board[i][j] == ' ') {
-            this.moveclear(i, j + 1);
-            this.move(i, j);
-            this.moves += 1;
+        i = cord[0]; j = cord[1] - 1;
+        if (j >= 0 && j <= 2 * this.M && this.Board[i][j] == ' ') {
+            this.moveclear(i, j + 1); this.move(i, j); this.moves += 1;
         }
-        else
-            return
     }
-
     this.movedown = function(id) {
         cord = this.checkPos(id);
-        var scale = this.S;
-        i = cord[0]
-        j = cord[1]
-        j += 1
-        if (j < 0)
-            return
-        else if (j > 2 * this.M)
-            return
-        else if (this.Board[i][j] == ' ') {
-            this.moveclear(i, j - 1);
-            this.move(i, j);
-            this.moves += 1;
+        i = cord[0]; j = cord[1] + 1;
+        if (j >= 0 && j <= 2 * this.M && this.Board[i][j] == ' ') {
+            this.moveclear(i, j - 1); this.move(i, j); this.moves += 1;
         }
-        else
-            return
     }
-
     this.moveleft = function(id) {
         cord = this.checkPos(id);
-        var scale = this.S;
-        i = cord[0]
-        j = cord[1]
-        i -= 1
-        if (i < 0)
-            return
-        else if (i > 2 * this.N)
-            return
-        else if (this.Board[i][j] == ' ') {
-            this.moveclear(i + 1, j);
-            this.move(i, j);
-            this.moves += 1;
+        i = cord[0] - 1; j = cord[1];
+        if (i >= 0 && i <= 2 * this.N && this.Board[i][j] == ' ') {
+            this.moveclear(i + 1, j); this.move(i, j); this.moves += 1;
         }
-        else
-            return
     }
-
     this.moveright = function(id) {
         cord = this.checkPos(id);
-        var scale = this.S;
-        i = cord[0]
-        j = cord[1]
-        i += 1
-        if (i < 0)
-            return
-        else if (i > 2 * this.N)
-            return
-        else if (this.Board[i][j] == ' ') {
-            this.moveclear(i - 1, j);
-            this.move(i, j);
-            this.moves += 1;
+        i = cord[0] + 1; j = cord[1];
+        if (i >= 0 && i <= 2 * this.N && this.Board[i][j] == ' ') {
+            this.moveclear(i - 1, j); this.move(i, j); this.moves += 1;
         }
-        else
-            return
     }
     this.checker = function(id) {
         cord = this.checkPos(id);
-        i = cord[0]
-        j = cord[1]
+        i = cord[0]; j = cord[1];
         if ((i == 19 && j == 20) || (i == 1 && j == 0)) {
             modelfunwin();
-            return 1
+            return 1;
         }
-        return 0
+        return 0;
     }
-    this.getMoves = function() {
-        return this.moves;
-    }
+    this.getMoves = function() { return this.moves; }
 };
 
-m = new maze(10, 10);
-m.init();
-m.add_edges();
-m.gen_maze();
-m.draw_canvas("canvas");
+
+
 function drawMoves() {
     document.getElementById("c").innerHTML = "Moves: " + m.getMoves()
 }
